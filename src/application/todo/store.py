d12934543712d9b024cc61bafa7df7c1ed02971f
weareuntitled <60941737+weareuntitled@users.noreply.github.com>
2026-01-27 13:11:21 +0100

    Add todo command and query tests

--- src/application/todo/store.py
+++ src/application/todo/store.py
@@ -4,6 +4,10 @@ from dataclasses import dataclass
 from datetime import datetime, timezone
 from itertools import count
 
+from infrastructure.data.repositories.in_memory_todo_repository import (
+    InMemoryTodoRepository,
+)
+
 
 @dataclass
 class TodoItem:
@@ -12,15 +16,15 @@ class TodoItem:
     created_at: datetime
 
 
-_TODO_ITEMS: list[TodoItem] = []
 _NEXT_ID = count(1)
+_REPOSITORY = InMemoryTodoRepository()
 
 
 def add_todo(title: str) -> TodoItem:
     item = TodoItem(id=next(_NEXT_ID), title=title, created_at=datetime.now(timezone.utc))
-    _TODO_ITEMS.append(item)
+    _REPOSITORY.add(item)
     return item
 
 
 def list_todos() -> list[TodoItem]:
-    return list(_TODO_ITEMS)
+    return list(_REPOSITORY.list())
